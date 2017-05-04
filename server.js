1f20e26ecaef25cdff128a8490e8afe14cde9a1a
Amit Bidwai <amitbidwai.26@gmail.com>
2017-05-04 17:37:11 +0530

    added cors and changed session secrate to external file

--- server.js
+++ server.js
@@ -8,17 +8,22 @@ var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
-
+var path = require('path');
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
 var configDB = require('./config/database.js');
-
+var cors = require('cors');
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
-
+mongoose.connection.on('connected', () =>{
+	console.log('connected')
+});
+mongoose.connection.on('error', (err) =>{
+	console.log('connected'+ err);
+});
 require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
@@ -26,12 +31,12 @@ app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors());
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
 app.use(session({
-    secret: 'reshmasecret', // session secret
+    secret: configDB.secret, // session secret
     resave: true,
     saveUninitialized: true
 }));
